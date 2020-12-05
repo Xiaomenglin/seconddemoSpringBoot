c2d19bdb7b68bb351e878943c4f710ee89b2a74d
Xiaomenglin <xmlin3171@163.com>
2020-12-05 09:47:41 +0800

    update pom

--- src/main/java/com/secondexample/SeconddemoApplication.java
+++ src/main/java/com/secondexample/SeconddemoApplication.java
@@ -3,10 +3,15 @@ package com.secondexample;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 @MapperScan("com.secondexample.mapper")
 public class SeconddemoApplication {
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SeconddemoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SeconddemoApplication.class, args);
