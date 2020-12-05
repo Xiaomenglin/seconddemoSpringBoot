1159bbe31ab3c422e8f721c8a0ead2fbd95182be
Xiaomenglin <xmlin3171@163.com>
2020-12-05 10:56:38 +0800

    update application.properties

--- src/main/java/com/secondexample/SeconddemoApplication.java
+++ src/main/java/com/secondexample/SeconddemoApplication.java
@@ -13,11 +13,6 @@ public class SeconddemoApplication {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(SeconddemoApplication.class);
     }
-    // 用来测试访问
-    @RequestMapping("/index")
-    public String home() {
-        return "hello 已启动项目";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SeconddemoApplication.class, args);
