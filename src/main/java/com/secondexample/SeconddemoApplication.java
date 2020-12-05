a3dd94e858e78680c13527a78a4347a76554cc12
Xiaomenglin <xmlin3171@163.com>
2020-12-05 10:41:52 +0800

    update pom

--- src/main/java/com/secondexample/SeconddemoApplication.java
+++ src/main/java/com/secondexample/SeconddemoApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @MapperScan("com.secondexample.mapper")
@@ -11,6 +12,11 @@ public class SeconddemoApplication {
 
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(SeconddemoApplication.class);
+    }
+    // 用来测试访问
+    @RequestMapping("/index")
+    public String home() {
+        return "hello 已启动项目";
     }
 
     public static void main(String[] args) {
